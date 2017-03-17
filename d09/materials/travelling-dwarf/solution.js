46391169b9aa4ec98bda72452eb4cf445fd7d90e
mepauta <mepauta@gmail.com>
2017-03-16 22:55:52 -0400

    changes

---
+++ d09/materials/travelling-dwarf/solution.js
@@ -0,0 +1,82 @@
+console.log("WRITE YOUR SOLUTION IN HERE!");
+
+// while (var y=0, x < 4;){
+//   d.move();
+// }
+
+x=0;
+while (x < 4) {
+  d.move();
+  x = x + 1;
+}
+
+
+
+d.orient("down"); 
+x=0;
+while (x < 17) {
+  d.move();
+  x = x +1;
+}
+
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.orient("down");
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.orient("right");
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.orient("up"),
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.orient("right")
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.orient("down")
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.orient("right")
+// d.move();
+// d.move();
+// d.move();
